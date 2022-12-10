8188e13df2a367e4f9d04b8d64d9ba12c3a7d16b
MuratYasul <113898547+MuratYasul@users.noreply.github.com>
2022-12-10 22:01:42 +0300

    deniyorum

---
+++ JavaProgramming/GitPractice/src/Test1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean scan1 = true;
+        System.out.println("Do you love me?");
+
+        if (scan1 == true)
+        {
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+        return;
+    }
+}
