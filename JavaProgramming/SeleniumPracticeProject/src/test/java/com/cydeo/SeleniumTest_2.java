87e994c82d69c1bed901a5a8f365428511352d6a
MuratYasul <113898547+MuratYasul@users.noreply.github.com>
2022-12-20 01:04:37 +0300

    Selenium deneme

---
+++ JavaProgramming/SeleniumPracticeProject/src/test/java/com/cydeo/SeleniumTest_2.java
@@ -0,0 +1,25 @@
+package com.cydeo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class SeleniumTest_2 {
+    public static void main(String[] args) {
+
+
+ WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.microsoft.com");
+//
+//        System.out.println("--------------------------");
+//
+//        WebDriverManager.operadriver().setup();
+//        WebDriver driver1 = new OperaDriver();
+//        driver1.get("https://www.opera.com");
+
+
+
+
+    }
+}
